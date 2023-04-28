01b89e6554fb341010b775f9e84de2b169358d1c
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2023-04-28 17:29:23 -0300

    Cobrindo testes de um serviço Aula 2

---
+++ java-testes-unitarios-tdd-projeto_inicial/src/test/java/tdd/junit/service/BonusServiceTest.java
@@ -0,0 +1,33 @@
+package tdd.junit.service;
+
+import tdd.junit.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+import tdd.junit.service.BonusService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
+        BonusService service = new BonusService();
+        BigDecimal bonus =service.calcularBonus(new Funcionario("Jesga", LocalDate.now(), new BigDecimal("25000")));
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario(){
+        BonusService service = new BonusService();
+        BigDecimal bonus =service.calcularBonus(new Funcionario("Jesga", LocalDate.now(), new BigDecimal("2500")));
+        assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoSeForAte10Mil(){
+        BonusService service = new BonusService();
+        BigDecimal bonus =service.calcularBonus(new Funcionario("Jesga", LocalDate.now(), new BigDecimal("10000")));
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+}
